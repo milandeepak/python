5b15ee766138881eff5e096fa01f7a735cab8c78
milandeepak <71485068+milandeepak@users.noreply.github.com>
2022-03-07 14:40:47 +0530

    update sieve.py

--- sieve.py
+++ sieve.py
@@ -1,14 +1,31 @@
+def Cumulative(l):
+   new = []
+   csum = 0
+   for element in l:
+      cumsum += element
+      new.append(csum)
+   return new 
+
 s=['80mm','40mm','20mm','10mm','4.75mm','2.36mm','1.18mm','600μm','300μm','150μm','Pan']
 w_s=[]
 for i in s:
   a=int(input('Enter the retained weight in '+i+' sieve:'))
   w_s.append(a)
+
 pw_s=[]
 for i in w_s:
  pw_s.append(i/10)
-print(pw_s)
+b=Cumulative(pw_s)
+
+sum=0
+for i in b:
+  sum=sum+i
 
 
+add=0
+for i in pw_s:
+    add=i+add
 
 
-
+fine=sum/add
+print('Fineness Modulus:'+str(fine))
